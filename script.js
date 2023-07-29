d61329c700c295d27ba7ea76a3117252bd95deb1
PavanCeaser <118599841+PavanCeaser@users.noreply.github.com>
2023-07-29 10:01:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript code can be added here for interactivity, but this example is empty.
+// For demonstration purposes, let's add a simple console log statement.
+
+console.log("JavaScript is working!");
